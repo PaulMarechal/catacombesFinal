e6cc003d30c81750b83c4d884bd897b7bc2a3757
Paul Marechal <paulmarechal785@hotmail.fr>
2023-01-11 22:14:58 +0100

    Online version - no bug

--- src/scroll.js
+++ src/scroll.js
@@ -1,37 +1,52 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
+// export function locomotiveScroll(){
+//     window.addEventListener("load", () => {
+//         const scroller = new LocomotiveScroll({
+//           el: document.querySelector("[data-scroll-container]"),
+//           smooth: true,
+//           multiplier: 0.75,
+//           scrollFromAnywhere: true,
+//         });
+//     });
+// }
+
 export function locomotiveScroll() {
-    const el = document.querySelector("[data-scroll-container]")
+    const el = document.querySelector("[data-scroll-container]");
     const scrollDivElement = document.getElementById("mouseScroll");
 
     if (el) {
-        const scroller = new LocomotiveScroll({
-        el: el,
-        smooth: true,
-        multiplier: 0.75,
-        scrollFromAnywhere: true,
+        window.addEventListener("load", () => {
+            const scroller = new LocomotiveScroll({
+              el: document.querySelector("[data-scroll-container]"),
+              smooth: true,
+              multiplier: 0.75,
+              scrollFromAnywhere: true,
+            });
+
+            scroller.on('scroll', ({ limit, scroll }) => {
+                const progress = scroll.y / limit.y * 100;
+                
+                if(progress <= 0.3){
+                    scrollDivElement.style.display="block";
+                    scrollDivElement.style.opacity = "1";
+                } else if (progress === 0 ){
+                    scrollDivElement.style.display="block";
+                    scrollDivElement.style.opacity = "1";
+                }else {
+                    scrollDivElement.style.transitionTiming="function: ease-in-out";
+                    scrollDivElement.style.transition= "opacity 1s";
+                    scrollDivElement.style.opacity = "0";
+                    scrollDivElement.style.transition="opacity 1s";
+    
+                    setTimeout(() => {
+                        scrollDivElement.style.display="none";
+                    }, 500);
+                }
+            });
         });
 
-        scroller.on('scroll', ({ limit, scroll }) => {
-            const progress = scroll.y / limit.y * 100;
-            
-            if(progress <= 0.3){
-                scrollDivElement.style.display="block";
-                scrollDivElement.style.opacity = "1";
-            } else if (progress === 0 ){
-                scrollDivElement.style.display="block";
-                scrollDivElement.style.opacity = "1";
-            }else {
-                scrollDivElement.style.transitionTiming="function: ease-in-out";
-                scrollDivElement.style.transition= "opacity 1s";
-                scrollDivElement.style.opacity = "0";
-                scrollDivElement.style.transition="opacity 1s";
-
-                setTimeout(() => {
-                    scrollDivElement.style.display="none";
-                }, 500);
-            }
-        });
+        
     } else {
         console.error("Il y a un probleme avec l'attribut : data-scroll-container");
     }
