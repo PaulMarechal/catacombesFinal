5a2080ef266c3c1713ebc1aa8dadf4520632f7d7
Paul Marechal <paulmarechal785@hotmail.fr>
2023-01-30 16:38:20 +0100

    Fixed bug AR ( display direct camera when click )

--- src/ar.js
+++ src/ar.js
@@ -4,19 +4,26 @@ export function arDisplay(linkAR){
 
     var elementA = document.createElement("a");
 
+    var elementImg = document.createElement("img")
+
     // If AR available on mobile
 
     if (elementA.relList.supports("ar")){
-        elementA.setAttribute("id", "arLink")
         elementA.setAttribute("rel", "ar");
+        elementA.setAttribute("id", "arLink");
         const parentDiv = document.getElementById("body");
 
         elementA.setAttribute("href", linkAR);
         elementA.style.color = "#FFFFFF"; 
-        elementA.textContent = "ENTER AR"
+        elementA.textContent = "ENTER AR" 
         elementDiv.setAttribute("id", "ARButton");
-        
+
+
+        elementImg.setAttribute("src", "https://catacombes.xyz/assets/images/couloirCatacombes.png");
+        elementImg.setAttribute("id", "imageAR");
+
         parentDiv.appendChild(elementDiv);
         elementDiv.appendChild(elementA);
+        elementA.appendChild(elementImg);
     }
 }
